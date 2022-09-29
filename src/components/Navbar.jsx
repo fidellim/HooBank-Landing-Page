0774919f37f255845dc10c0e7fd21d4d0395f89a
fidellim <lim.fidel@gmail.com>
2022-09-29 22:02:53 +0400

    add hover effect on navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,14 @@ const Navbar = () => {
     const [toggle, setToggle] = useState(false)
 
     return (
-        <nav className="w-full flex py-6 justify-between items-center navbar">
+        <nav className="w-full flex py-6 justify-between items-center">
             <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
 
             <ul className="list-none sm:flex hidden justify-end items-center flex-1">
                 {navLinks.map((nav, index) => (
                     <li
                         key={nav.id}
-                        className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                        className={`font-poppins font-normal cursor-pointer text-[16px] hover:text-white ${
                             active === nav.title
                                 ? 'text-white'
                                 : 'text-dimWhite'
@@ -44,7 +44,7 @@ const Navbar = () => {
                         {navLinks.map((nav, index) => (
                             <li
                                 key={nav.id}
-                                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                                className={`font-poppins font-medium cursor-pointer text-[16px] hover:text-white ${
                                     active === nav.title
                                         ? 'text-white'
                                         : 'text-dimWhite'
