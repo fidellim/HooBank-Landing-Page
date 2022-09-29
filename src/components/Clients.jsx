6d26aaa141d5d6b7a91de961b17910eb4e3815dd
fidellim <lim.fidel@gmail.com>
2022-09-29 14:16:27 +0400

    add clients

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { clients } from '../constants'
+import styles from '../style'
 
-const Clients = () => {
-    return <div>Clients</div>
-}
+const Clients = () => (
+    <section className={`${styles.flexCenter} my-4`}>
+        <div className={`${styles.flexCenter} flex-wrap gap-8 w-full`}>
+            {clients.map((client) => (
+                <div
+                    key={client.id}
+                    className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+                >
+                    <img
+                        src={client.logo}
+                        alt="client_logo"
+                        className="sm:w-[192px] w-[100px] object-contain client"
+                    />
+                </div>
+            ))}
+        </div>
+    </section>
+)
 
 export default Clients
